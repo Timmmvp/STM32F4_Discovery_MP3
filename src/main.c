b8e2d23dfc0ef1de04ee9ad342912add922a9fe8
Timmmvp <tim.van.putten.tvp@gmail.com>
2017-01-25 12:08:00 +0100

    first commentary
    my first commentary for code explanation

--- src/main.c
+++ src/main.c
@@ -140,12 +140,13 @@ void init() {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
 	// Configure PD12, PD13, PD14 and PD15 in output pushpull mode
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;	// refers to pins 12 up & till 15 as pins he's talking to and gives this value to the struct GPIO_InitStructure.
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;										// sets pins 12 up & till 15 as outputs and gives this value to the struct GPIO_InitStructure.
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;										// sets pushpull config for those pins and gives this value to the struct GPIO_InitStructure.
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;									// sets pinspeed and gives this value to the struct GPIO_InitStructure.
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;									// choose no pull resistor and gives this value to the struct GPIO_InitStructure.
+	GPIO_Init(GPIOD, &GPIO_InitStructure);												// initialises port D with the values given to GPIO_InitStructure
+
 
 
 	// ------ UART ------ //
